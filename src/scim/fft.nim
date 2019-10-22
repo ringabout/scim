830ca94fc1fc396d233242b2804aa683c38ec638
flywind <xzsflywind@gmail.com>
2019-10-22 20:05:26 +0800

    update

--- src/scim/fft.nim
+++ src/scim/fft.nim
@@ -1,72 +1,14 @@
-import math, complex, timeit, sequtils, sugar, strutils
+import math, complex, timeit, sugar, arraymancer, sequtils
  
-# Works with floats and complex numbers as input
-proc fft1[T: float | Complex64](x: openarray[T]): seq[Complex64] {.discardable.} =
-  # 获取长度
-  let n = x.len
-  if n == 0: return
- 
-  result.newSeq(n)
- 
-  if n == 1:
-    result[0] = (when T is float: complex(x[0]) else: x[0])
-    return
- 
-  var evens, odds = newSeq[T]()
-  for i, v in x:
-    if (i and 1) == 0: evens.add v
-    else: odds.add v
-  var 
-    (even, odd) = (fft1(evens), fft1(odds))
-    w = complex(1.0)
-    wm = exp(complex(0.0, -2 * Pi / float(n)))
-  let halfn = n shr 1
- 
-  for k in 0 ..< halfn:
-    let a = w * odd[k]
-    result[k] = even[k] + a
-    result[k + halfn] = even[k] - a
-    w *= wm
 
-
-
-proc ifftAid(x: seq[Complex64]): seq[Complex64] =
-  let n = x.len
-  if n == 0: return
-
-  result.newSeq(n)
-
-  if n == 1: 
-    result[0] = x[0]
-    return
-
-  var evens, odds = newSeq[Complex64]()
-  for i, v in x:
-    if (i and 1) == 0: evens.add v
-    else: odds.add v
-  var
-    (even, odd) = (ifftAid(evens), ifftAid(odds))
-    w = complex(1.0)
-    wm = exp(complex(0.0, 2 * Pi / float(n)))
-  let half = n shr 1
-  for k in 0 ..< half:
-    let a = w * odd[k]
-    result[k] = (even[k] + a) 
-    result[k + half] = (even[k] - a) 
-    w *= wm
-
-proc ifft(x: seq[Complex64]): seq[Complex64] =
-  return x.ifftAid.map(item => item / x.len.float)
   
-  
-
-
-
 type
-  ComplexType = float | Complex64
+  ComplexType = Complex[float64] | Complex[float32]
 
 
-proc bitReverseCopy[T: ComplexType](x: seq[T]): seq[Complex64] = 
+
+
+proc bitReverseCopy[T: ComplexType](x: seq[T]): seq[T] = 
   let n = x.len
   var mid = x
   var
@@ -84,40 +26,81 @@ proc bitReverseCopy[T: ComplexType](x: seq[T]): seq[Complex64] =
       k = k shr 1
     j += k
   for item in mid:
-    result.add complex(item)
+    result.add item
 
 
 
-
-proc fft[T: ComplexType](x: seq[T]): seq[Complex64] {.discardable.} =
-  let n = x.len
-  var a = bitReverseCopy(x)
-  for s in 1 .. int(log2(n.float32)):
+proc fftAid[T: ComplexType](x: seq[T], flag: float = -1): Tensor[T] =
+  var 
+    n = x.len
+    n1 = int(log2(n.float32))
+    n2 = 2 ^ n1
+    padding_length: int
+    temp = x
+  if n != n2:
+    padding_length = 2 * n2 - n
+    n = 2 * n2
+    n1 += 1
+  for i in 1 .. padding_length:
+    temp.add(complex(0.0))
+  temp = bitReverseCopy[T](temp)
+  for s in 1 .. n1:
     let 
       m = 2 ^ s
-      wm = exp(complex(0.0, -2 * Pi / float(m)))
+      # flag * 2 * Pi 
+      wm = exp(complex(0.0, flag * 2.0 * Pi / float(m)))
+ 
     for k in countup(0, n - 1 , m):
       var w = complex(1.0)
       let m2 = m div 2
       for j in 0 ..< m2:
         let 
-          t = w * a[k + j + m2]
-          u = a[k + j]
-        a[k + j] = u + t
-        a[k + j + m2] = u - t
+          t = w * temp[k + j + m2]
+          u = temp[k + j]
+        temp[k + j] = u + t
+        temp[k + j + m2] = u - t
         w = w * wm
-  return a
+  return temp.toTensor
 
-import arraymancer
+proc fft*[T:ComplexType](x: seq[T] | Tensor[T]): Tensor[Complex[float64]] =
+  var temp: seq[T]
+  when x is seq:
+    temp = x
+  elif x is Tensor:
+    temp = x.toRawSeq
+  # when T is SomeFloat:
+  #   temp = temp.map(x=>complex(x))
+  result = temp.fftAid(-1)
+
+proc fft*[T: SomeFloat](x: seq[T] | Tensor[T]): Tensor[Complex[float64]] =
+  result = fft(x.map(t=>t.complex))
+
+proc ifft*[T:ComplexType](x: seq[T] | Tensor[T]): Tensor[Complex[float64]] =
+  var temp: seq[T]
+  when x is seq:
+    temp = x
+  elif x is Tensor:
+    temp = x.toRawSeq
+  # when T is SomeFloat:
+  #   temp = temp.map(x=>complex(x))
+  result = temp.fftAid(1).map(item => item / temp.len.float64)
+
+proc ifft*[T: SomeFloat](x: seq[T] | Tensor[T]): Tensor[Complex[float64]] =
+  result = ifft(x.map(t=>t.complex))
+
+var a = @[1.0, 1.0, 1.0, 1.0, 0.0, 0.0].map(x=>complex(x))
+var b = @[1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0].map(x=>complex(x))
+var c = @[1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0].map(x=>complex(x))
+# echo a
+echo ifft(fft(a))
+echo ifft(fft(b))
+echo ifft(fft(c))
 
 
 # echo timeGo(fft(@[1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0]))
 # echo timeGo(fft1(@[1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0]))
 
 
-var aaa: seq[float] = @[]
-for i in 1 .. 2048:
-  aaa.add(float(i))
 
 # echo timeGo(fft(aaa))
 # echo timeGo(fft1(aaa))
